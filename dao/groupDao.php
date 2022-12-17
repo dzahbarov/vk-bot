2aaeaaa6886ca16d85c56d3cd83da525a57ca9cd
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-17 23:30:26 +0300

    add exams

--- dao/groupDao.php
+++ dao/groupDao.php
@@ -1,6 +1,7 @@
 <?php
 
-function add_group($user_id, $group_name) {
+function add_group($user_id, $group_name)
+{
     bot_sendMessage($user_id, "2");
     $servername = "localhost";
     $username = "bot";
@@ -12,17 +13,41 @@ function add_group($user_id, $group_name) {
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
         bot_sendMessage($user_id, "3");
-    } catch(PDOException $e) {
+    } catch (PDOException $e) {
         bot_sendMessage($user_id, $e->getMessage());
-
         echo "Connection failed: " . $e->getMessage();
     }
     bot_sendMessage($user_id, "4");
-    $stmt= $conn->prepare("INSERT INTO group_table (user_vk_id, group_name) VALUES (:user_vk_id,:group_name)");
+    $stmt = $conn->prepare("INSERT INTO group_table (user_vk_id, group_name) VALUES (:user_vk_id,:group_name)");
     $stmt->execute([
         'user_vk_id' => $user_id,
         'group_name' => $group_name
     ]);
     bot_sendMessage($user_id, "6");
+}
+
+function get_group($user_id)
+{
+    $servername = "localhost";
+    $username = "bot";
+    $password = "@Aasdjkhkuhb43289b";
+    $conn = null;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=bot_db", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+        bot_sendMessage($user_id, "3");
+    } catch (PDOException $e) {
+        bot_sendMessage($user_id, $e->getMessage());
+        echo "Connection failed: " . $e->getMessage();
+    }
+    bot_sendMessage($user_id, "4");
+    $stmt = $conn->prepare("SELECT group_name from group_table where user_vk_id=:user_vk_id");
+    $stmt->execute([
+        'user_vk_id' => $user_id
+    ]);
+
+    return $stmt->fetch()[0]['group_name'];
 }
 
--- dao/initDB.sql
+++ dao/initDB.sql
@@ -2,11 +2,81 @@ CREATE DATABASE IF NOT EXISTS bot_db;
 
 use bot_db;
 
-create table group_table
+create table Students
 (
-    user_vk_id int not null,
-    group_name tinytext not null
+    user_vk_id int PRIMARY KEY,
+    group_id int
 );
+
+create table 'Groups'
+(
+    group_id int PRIMARY KEY AUTO_INCREMENT,
+    group_no char(6)
+);
+
+create table Exams
+(
+    group_id int,
+    subject_id int,
+    ts timestamp null
+);
+
+create table Subjects
+(
+    subject_id int PRIMARY KEY AUTO_INCREMENT,
+    subject_name varchar(50),
+    lector varchar(50),
+    exam boolean
+);
+
+alter table Students
+    add constraint student_group_id
+    foreign key (group_id) references `Groups`(group_id);
+
+alter table Exams
+    add constraint Exams_subject_id
+        foreign key (subject_id) references `Subjects`(subject_id);
+
+
+insert into `Groups`(group_no) values ('M33371');
+
+insert into Subjects(subject_name, lector) values
+    ('Анализ данных', 'Муравьёв Сергей Борисович'),
+    ('Методы трансляции', 'Станкевич Андрей Сергеевич'),
+    ('Параллельное программирование', 'Елизаров Роман Анатольевич'),
+    ('Математическая статистика', 'Блаженов Алексей Викторович'),
+    ('Функциональное программирование', 'Serokell'),
+    ('PHP', 'VK');
+
+
+
+
+
+# create table schedule_table_help
+# (
+#     group_name tinytext not null,
+#     schedule_id int not null
+# );
+
+# create table schedule_table
+# (
+#     schedule_id int not null,
+#     weekday int not null,
+#     day_schedule_id int not null
+# );
+
+# create table day_schedule
+# (
+#     day_schedule_id int not null,
+#     slot0;
+#     slot1;
+#     slot2;
+#     slot3;
+#     slot4
+# )
+
+
+
 
 # insert into test_table(test_col)
 # values (42);

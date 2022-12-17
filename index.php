61e8e14b068c0810b881d33dd952dd5302725522
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-17 20:39:30 +0300

    check connect

--- index.php
+++ index.php
@@ -12,59 +12,79 @@ require_once 'api/yandex_api.php';
 require_once 'bot/bot.php';
 
 if (!isset($_REQUEST)) {
-  exit;
+    exit;
 }
 
 callback_handleEvent();
 
-function callback_handleEvent() {
-  $event = _callback_getEvent();
+function callback_handleEvent()
+{
+    $event = _callback_getEvent();
 
-  try {
-    switch ($event['type']) {
-      //Подтверждение сервера
-      case CALLBACK_API_EVENT_CONFIRMATION:
-        _callback_handleConfirmation();
-        break;
+    try {
+        switch ($event['type']) {
+            //Подтверждение сервера
+            case CALLBACK_API_EVENT_CONFIRMATION:
+                _callback_handleConfirmation();
+                break;
 
-      //Получение нового сообщения
-      case CALLBACK_API_EVENT_MESSAGE_NEW:
-        _callback_handleMessageNew($event['object']);
-        break;
+            //Получение нового сообщения
+            case CALLBACK_API_EVENT_MESSAGE_NEW:
+                _callback_handleMessageNew($event['object']);
+                break;
 
-      default:
-        _callback_response('Unsupported event');
-        break;
+            default:
+                _callback_response('Unsupported event');
+                break;
+        }
+    } catch (Exception $e) {
+        log_error($e);
     }
-  } catch (Exception $e) {
-    log_error($e);
-  }
 
-  _callback_okResponse();
+    _callback_okResponse();
 }
 
-function _callback_getEvent() {
-  return json_decode(file_get_contents('php://input'), true);
+function _callback_getEvent()
+{
+    return json_decode(file_get_contents('php://input'), true);
 }
 
-function _callback_handleConfirmation() {
-  _callback_response(CALLBACK_API_CONFIRMATION_TOKEN);
+function _callback_handleConfirmation()
+{
+    _callback_response(CALLBACK_API_CONFIRMATION_TOKEN);
 }
 
-function _callback_handleMessageNew($data) {
-  $user_id = $data['message']['from_id'];
-  $text = $data['message']['text'];
-  bot_sendMessage($user_id, $text);
-  _callback_okResponse();
+function _callback_handleMessageNew($data)
+{
+    $user_id = $data['message']['from_id'];
+    $text = $data['message']['text'];
+
+    $servername = "localhost";
+    $username = "root";
+//    $password = "password";
+    $dbname = "test_db";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        bot_sendMessage($user_id, "Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
+    }
+    bot_sendMessage($user_id, "Connected successfully");
+    bot_sendMessage($user_id, $text);
+    _callback_okResponse();
 }
 
-function _callback_okResponse() {
-  _callback_response('ok');
+function _callback_okResponse()
+{
+    _callback_response('ok');
 }
 
-function _callback_response($data) {
-  echo $data;
-  exit();
+function _callback_response($data)
+{
+    echo $data;
+    exit();
 }
 
 

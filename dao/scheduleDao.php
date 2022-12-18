90bd09cb4c272b62206f3b8027bb1d29368305fc
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-18 19:33:06 +0300

    check

--- dao/scheduleDao.php
+++ dao/scheduleDao.php
@@ -2,7 +2,6 @@
 
 function get_schedule($user_id, $group_id, $weekday)
 {
-    bot_sendMessage($user_id, "34");
     $servername = "localhost";
     $username = "bot";
     $password = "@Aasdjkhkuhb43289b";
@@ -17,7 +16,6 @@ function get_schedule($user_id, $group_id, $weekday)
         bot_sendMessage($user_id, $e->getMessage());
         echo "Connection failed: " . $e->getMessage();
     }
-    bot_sendMessage($user_id, "1oo");
     $stmt = $conn->prepare("select Classes.subject_id, Subjects.subject_name,  start_class, end_class from Schedule left join Classes on class1_id=class_id or class2_id=class_id or class3_id=class_id or class4_id=class_id or class5_id=class_id or class6_id=class_id or class7_id=class_id inner join Subjects on Classes.subject_id=Subjects.subject_id where weekday=:weekday and Schedule.group_id=:group_id");
 
     $stmt->execute([
--- dao/subjectsDao.php
+++ dao/subjectsDao.php
@@ -2,7 +2,7 @@
 
 function get_subjects($user_id, $group_id)
 {
-    bot_sendMessage($user_id, "34");
+
     $servername = "localhost";
     $username = "bot";
     $password = "@Aasdjkhkuhb43289b";
@@ -17,7 +17,7 @@ function get_subjects($user_id, $group_id)
         bot_sendMessage($user_id, $e->getMessage());
         echo "Connection failed: " . $e->getMessage();
     }
-    bot_sendMessage($user_id, "1oo");
+
     $stmt = $conn->prepare("select * from Subjects where Subjects.group_id=:group_id");
 
     $stmt->execute([
@@ -30,7 +30,7 @@ function get_subjects($user_id, $group_id)
 
 function get_useful_links($user_id, $subject_id)
 {
-    bot_sendMessage($user_id, "34");
+
     $servername = "localhost";
     $username = "bot";
     $password = "@Aasdjkhkuhb43289b";
@@ -45,7 +45,7 @@ function get_useful_links($user_id, $subject_id)
         bot_sendMessage($user_id, $e->getMessage());
         echo "Connection failed: " . $e->getMessage();
     }
-    bot_sendMessage($user_id, "1oo");
+
     $stmt = $conn->prepare("select * from Useful inner join Subjects on Useful.subject_id = Subjects.subject_id where Subjects.subject_id=:subject_id");
 
     $stmt->execute([

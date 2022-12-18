027f10bc917d4813a62713683a6a42e0502a0115
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-18 04:51:15 +0300

    check

--- dao/initDB.sql
+++ dao/initDB.sql
@@ -25,7 +25,29 @@ create table Subjects
 (
     subject_id int AUTO_INCREMENT PRIMARY KEY,
     subject_name varchar(50),
-    lector varchar(50)
+    lector varchar(50),
+    location varchar(30)
+);
+
+create table Schedule
+(
+    group_id int,
+    weekday char(3),
+    class1_id int,
+    class2_id int,
+    class3_id int,
+    class4_id int,
+    class5_id int,
+    class6_id int,
+    class7_id int
+);
+
+create table Classes
+(
+  class_id int AUTO_INCREMENT PRIMARY KEY,
+  subject_id int,
+  start_class Time,
+  end_class Time
 );
 
 alter table Students
@@ -33,8 +55,37 @@ alter table Students
     foreign key (group_id) references `Groups`(group_id);
 
 alter table Exams
-    add constraint Exams_subject_id
+    add constraint exams_subject_id
         foreign key (subject_id) references `Subjects`(subject_id);
+
+alter table Schedule
+    add constraint schedule_class1_id
+        foreign key (class1_id) references `Classes`(class_id);
+
+alter table Schedule
+    add constraint schedule_class2_id
+        foreign key (class2_id) references `Classes`(class_id);
+
+alter table Schedule
+    add constraint schedule_class3_id
+        foreign key (class3_id) references `Classes`(class_id);
+
+alter table Schedule
+    add constraint schedule_class4_id
+        foreign key (class4_id) references `Classes`(class_id);
+
+alter table Schedule
+    add constraint schedule_class5_id
+        foreign key (class5_id) references `Classes`(class_id);
+
+alter table Schedule
+    add constraint schedule_class6_id
+        foreign key (class6_id) references `Classes`(class_id);
+
+alter table Schedule
+    add constraint schedule_class7_id
+        foreign key (class7_id) references `Classes`(class_id);
+
 
 insert into `Groups`(group_no) values ('M33371');
 
@@ -50,8 +101,14 @@ insert into Subjects(subject_name, lector) values
 insert into Exams(group_id, subject_id, ts) values
     (1, 2, '2022-01-13'),
     (1, 3, '2022-01-17'),
-    (1, 4, '2022-01-23')
+    (1, 4, '2022-01-23');
 
+# insert into Classes(subject_id, start_class, end_class) values
+#     (4, '11:40:00', '13:10:00'),
+#     (4, '15:20:00', '16:40:00'),
+#     (3, '17:00:00', '18:20:00'),
+#     (3, '18:40:00', '20:00:00')
+#     (2, '13:30:')
 
 
 

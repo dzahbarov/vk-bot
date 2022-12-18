c35f87bf3db0dc02967955ffc56cdd0d892462f4
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-18 16:59:20 +0300

    fx links

--- index.php
+++ index.php
@@ -134,6 +134,7 @@ function _callback_handleMessageNew($data)
 
     bot_sendMessage($user_id, $payload->button);
     if ($payload != null && str_starts_with($payload->button, "Subject")) {
+        bot_sendMessage($user_id, 33);
         $args = explode(" ", $payload->button);
         $subject_id = end($args);
 

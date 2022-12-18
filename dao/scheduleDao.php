af25ef16ecc59bf0749851307b3e38bf5953dd51
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-18 19:15:48 +0300

    check

--- dao/scheduleDao.php
+++ dao/scheduleDao.php
@@ -18,7 +18,7 @@ function get_schedule($user_id, $group_id, $weekday)
         echo "Connection failed: " . $e->getMessage();
     }
     bot_sendMessage($user_id, "1oo");
-    $stmt = $conn->prepare("select Classes.subject_id, Subjects.subject_name,  start_class, end_class from Schedule left join Classes on class1_id=class_id or class2_id=class_id or class3_id=class_id or class4_id=class_id or class5_id=class_id or class6_id=class_id or class7_id=class_id inner join Subjects on Classes.subject_id=Subjects.subject_id where weekday=:weekday and group_id=:group_id");
+    $stmt = $conn->prepare("select Classes.subject_id, Subjects.subject_name,  start_class, end_class from Schedule left join Classes on class1_id=class_id or class2_id=class_id or class3_id=class_id or class4_id=class_id or class5_id=class_id or class6_id=class_id or class7_id=class_id inner join Subjects on Classes.subject_id=Subjects.subject_id where weekday=:weekday and Schedule.group_id=:group_id");
 
     $stmt->execute([
         'weekday' => $weekday,

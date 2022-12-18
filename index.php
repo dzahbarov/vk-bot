0367c51a105ba79fd995619cb60d91bd023e57b0
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-18 03:04:44 +0300

    check

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ function _callback_handleMessageNew($data)
 //            bot_sendMessage($user_id, "2");
 //            bot_sendMessage($user_id, $exam['subject_name']);
 //            bot_sendMessage($user_id, $ans);
-            $ans = $ans . $exam['subject_name'] . $exam['ts'] . "\n";
+            $ans = $ans . $exam['subject_name'] . ' ' .  $exam['ts'] . "\n";
         }
 //        bot_sendMessage($user_id, "3");
         vkApi_messagesSend($user_id, $ans);

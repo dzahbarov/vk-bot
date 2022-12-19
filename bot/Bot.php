1d07426f9fb18c08b42f14b99fa4e4a2c1db5f59
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-19 13:43:12 +0300

    check

--- bot/Bot.php
+++ bot/Bot.php
@@ -226,6 +226,7 @@ class Bot
         $this->vk_api->sendMessage($user_id, 123);
         $key = json_decode(file_get_contents("bot/keyboards/add_group.json"), true);
         $this->vk_api->sendMessage($user_id, 345);
+        $this->vk_api->sendMessage($user_id, $key);
         $this->vk_api->sendMessageWithKeyboard($user_id, "Выберите группу", $key);
         $this->vk_api->sendMessage($user_id, 3444);
         _callback_okResponse();

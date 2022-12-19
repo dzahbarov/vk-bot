fe60809bda5dc641f710e7c49cf13df795c61632
dzahbarov <dzahbarovvladimir@gmail.com>
2022-12-19 03:22:53 +0300

    fx

--- dao/AbstractDao.php
+++ dao/AbstractDao.php
@@ -8,9 +8,12 @@ abstract class AbstractDao
     final public function __construct()
     {
         $servername = BD_SERVERNAME;
+        $database = BD_DATABASE;
+        $username = BD_USERNAME;
+        $password = BD_PASSWORD;
         $conn = null;
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=" . BD_DATABASE, BD_USERNAME, BD_PASSWORD);
+            $conn = new PDO("mysql:host=$servername;dbname=" . $database, $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
